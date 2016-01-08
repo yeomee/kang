5d44a279c622f6e588aaf1b4595f3cd2c0e484fd
oyg <collme74@gmail.com>
2016-01-08 12:15:07 +0900

    회원 리스트 구현

--- app/src/main/java/com/eastflag/kang/Constant.java
+++ app/src/main/java/com/eastflag/kang/Constant.java
@@ -9,4 +9,5 @@ public final class Constant {
     public static final String API_INTRO= "/intro.asp";
     public static final String API_010 = "/moim/moim.asp";
     public static final String API_020 = "/moim/moim_i.asp ";
+    public static final String API_012 = "/member/member.asp";
 }
